19b6c297f1da893145c4945ba177af10a08e3d10
Victor Olatunji <victoluolatunji@gmail.com>
2024-03-28 11:05:03 +0100

    codebase setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "eagle-lake": ["Eagle Lake", "serif"],
+        inter: ["Inter", "sans-serif"],
+        lit: ["Literata", "sans-serif"],
+        dot: ["DotGothic16", "sans-serif"],
+        dosis: ["Dosis", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
