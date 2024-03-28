a6d43719e0b4ba23d97ac22fafe9f4d937f93264
Victor Olatunji <victoluolatunji@gmail.com>
2024-03-28 11:55:31 +0100

    skeletoned

---
+++ src/config/index.ts
@@ -0,0 +1,22 @@
+export const META_TITLE = "Vistapay";
+export const META_DESCRIPTION =
+  "Vistapay is an VTU and payment innovation for Africa!";
+export const META_IMAGE =
+  "https://res.cloudinary.com/dkoe20rzl/image/upload/v1697031657/kdo4n6qj2paglcdr5vbd.png";
+export const META_IMAGE_ALT = "Vistapay Logo";
+export const META_IMAGE_WIDTH = "1200";
+export const META_IMAGE_HEIGHT = "630";
+export const META_IMAGE_TYPE = "image/png";
+export const META_IMAGE_QUALITY = "100";
+export const META_IMAGE_URL_ALT = "Vistapay Logo";
+export const META_IMAGE_WIDTH_ALT = "1200";
+export const META_IMAGE_HEIGHT_ALT = "630";
+export const META_IMAGE_TYPE_ALT = "image/png";
+export const META_IMAGE_QUALITY_ALT = "100";
+export const META_URL = "https://Vistapay.brimble.app/"; //will change to  the real domain later
+export const META_TYPE = "website";
+export const META_AUTHOR = "Vistapay Dev Team";
+export const META_PUBLISHER = "Vistapay Inc";
+export const META_PUBLISHED_AT = "2023-09-27";
+export const META_KEYWORDS =
+  "Vistapay, VTU, Airtime, Data, MTN, GLO, Airtel,payment, Flutterwave, MoniePoint ";
